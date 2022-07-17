0a023a39803865a518243c84d0ea21ae510ba344
jangjunha <hi@jangjunha.me>
2022-07-18 00:34:02 +0900

    feat: make timetable page reactive to change

--- src/pages/UserPage/pages/TimetablePage/queries/useTimetable.ts
+++ src/pages/UserPage/pages/TimetablePage/queries/useTimetable.ts
@@ -1,4 +1,4 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, onSnapshot } from 'firebase/firestore';
 import { fold } from 'fp-ts/lib/Either';
 import { identity, pipe } from 'fp-ts/lib/function';
 import { useEffect, useState } from 'react';
@@ -20,46 +20,39 @@ export const useTimetable = (
     stage: 'loading',
   });
 
-  useEffect(() => {
-    let isUnmounted = false;
-    async function fetch(): Promise<void> {
-      const snapshot = await getDoc(
-        doc(db, 'users', userID, 'timetables', timetableID)
-      );
-      if (isUnmounted) {
-        return;
-      }
-      if (!snapshot.exists()) {
-        setFetchState({
-          stage: 'error',
-          message: '시간표를 찾을 수 없습니다.',
-        });
-        return;
-      }
+  useEffect(
+    () =>
+      onSnapshot(
+        doc(db, 'users', userID, 'timetables', timetableID),
+        (snapshot) => {
+          if (!snapshot.exists()) {
+            setFetchState({
+              stage: 'error',
+              message: '시간표를 찾을 수 없습니다.',
+            });
+            return;
+          }
 
-      const data = pipe(
-        timetableCodec.decode(snapshot.data()),
-        fold((errors) => {
-          console.error(errors);
-          throw new Error('decode error');
-        }, identity)
-      );
-      if (data == null) {
-        setFetchState({
-          stage: 'error',
-          message: '시간표 정보를 처리하는 중 오류가 발생했습니다.',
-        });
-        throw new Error('Cannot decode timetable');
-      }
+          const data = pipe(
+            timetableCodec.decode(snapshot.data()),
+            fold((errors) => {
+              console.error(errors);
+              throw new Error('decode error');
+            }, identity)
+          );
+          if (data == null) {
+            setFetchState({
+              stage: 'error',
+              message: '시간표 정보를 처리하는 중 오류가 발생했습니다.',
+            });
+            throw new Error('Cannot decode timetable');
+          }
 
-      setFetchState({ stage: 'fetched', id: snapshot.id, timetable: data });
-    }
-    setFetchState({ stage: 'loading' });
-    fetch();
-    return () => {
-      isUnmounted = true;
-    };
-  }, [userID, timetableID]);
+          setFetchState({ stage: 'fetched', id: snapshot.id, timetable: data });
+        }
+      ),
+    [userID, timetableID]
+  );
 
   return fetchState;
 };
